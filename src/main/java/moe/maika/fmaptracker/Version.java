bb7fdc7b5d48bf7ee920a3f8e6444f7eea1a1473
sg4e <46875746+sg4e@users.noreply.github.com>
2024-09-25 00:45:25 +0100

    Bump version for release

--- src/main/java/moe/maika/fmaptracker/Version.java
+++ src/main/java/moe/maika/fmaptracker/Version.java
@@ -19,7 +19,7 @@
 package moe.maika.fmaptracker;
 
 public class Version {
-    private static final String VERSION_STRING = "v2.3.2-SNAPSHOT";
+    private static final String VERSION_STRING = "v2.3.2";
 
     private static final int MAJOR, MINOR, PATCH;
 
